4b1b1e494321e2f28097497c612dfa43a4cfbccc
Brody Foster <brody.foster@gmail.com>
2025-07-19 16:18:42 -0400

    added pickup message

--- entities/pickups/weapon_pickup.gd
+++ entities/pickups/weapon_pickup.gd
@@ -2,12 +2,14 @@ extends Area3D
 class_name WeaponPickup
 
 @export var weapon_scene: PackedScene = preload("res://weapons/crossbow.tscn")
+@export var pickup_message: String = "You pick up something strange..."
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	if body is Player:
+		GameManager.send_message(pickup_message)
 		body = body as Player
 		body.pickup_weapon(weapon_scene) 
 		queue_free()
